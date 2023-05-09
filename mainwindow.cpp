0f36ec4b76de8aefe3c4049f762a52936ba23844
unknown <zhikangliu1234@gmail.com>
2023-05-09 15:39:37 -0700

    design mainwindow interface

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -6,6 +6,14 @@ MainWindow::MainWindow(QWidget *parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    QPixmap pix(":/img/database.jpg");
+    int w = ui->label->width();
+    int h = ui->label->height();
+    ui->label->setPixmap(pix.scaled(w,h,Qt::KeepAspectRatio));
+    QFont font = ui->title->font();
+    font.setBold(true);
+    font.setPointSize(20);
+    ui->title->setFont(font);
 }
 
 MainWindow::~MainWindow()
--- mainwindow.h
+++ mainwindow.h
@@ -2,6 +2,7 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QPixmap>
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
 QT_END_NAMESPACE
